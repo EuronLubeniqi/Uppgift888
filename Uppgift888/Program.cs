453336e65c3ccedaf899f74eebbcd8199d1cd760
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-12-17 13:49:58 +0100

    Add project files.

---
+++ Uppgift888/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift888
+{
+    class Program
+    {
+        static void Main()
+        {
+            int produkt = 1;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                produkt *= i; 
+            }
+
+            Console.WriteLine("Produkten av heltalen från 1 till 5 är: " + produkt);
+        }
+    }
+}
